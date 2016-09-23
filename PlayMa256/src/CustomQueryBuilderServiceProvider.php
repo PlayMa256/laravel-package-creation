1ec67c7b1a6ad14c93fdbfb95dbdf27b41d7b354
PlayMa256 <matheus.g.silva@hotmail.com>
2016-09-23 13:26:05 -0300

    composer updated

--- PlayMa256/src/CustomQueryBuilderServiceProvider.php
+++ PlayMa256/src/CustomQueryBuilderServiceProvider.php
@@ -1,7 +1,9 @@
 <?php
+namespace PlayMa256\CustomQuery;
+use Illuminate\Support\ServiceProvider;
 class CustomQueryBuilderServiceProvider extends ServiceProvider{
 	public function boot(){
-
+        
 	}
 	public function register(){
 		
